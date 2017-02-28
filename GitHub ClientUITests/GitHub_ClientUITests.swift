56371a9fac1764cc6f24208de371b480ee45ad33
Arturo Jamaica <admin@example.com>
2017-02-28 11:01:44 -0600

    Finish all UI tests

--- GitHub ClientUITests/GitHub_ClientUITests.swift
+++ GitHub ClientUITests/GitHub_ClientUITests.swift
@@ -1,3 +1,4 @@
+
 //
 //  GitHub_ClientUITests.swift
 //  GitHub ClientUITests
@@ -19,6 +20,7 @@ class GitHub_ClientUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
+        
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,9 +30,54 @@ class GitHub_ClientUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSearch(){
+        
+    
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        tablesQuery.searchFields["Search"].tap()
+        app.searchFields["Search"].typeText("jquer")
+        sleep(5)
+        XCTAssert(tablesQuery.staticTexts["jquery"].exists)
+
     }
+    
+    func testSeeCommits() {
+
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery.cells.containing(.staticText, identifier:"grit").staticTexts["mojombo"].tap()
+        tablesQuery.staticTexts["Commits"].tap()
+        
+    }
+    
+    
+    func testSeeIssues() {
+        
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery.cells.containing(.staticText, identifier:"grit").staticTexts["mojombo"].tap()
+        tablesQuery.staticTexts["Issues"].tap()
+
+        
+    }
+    
+    func testSeePull() {
+        
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery.cells.containing(.staticText, identifier:"grit").staticTexts["mojombo"].tap()
+        tablesQuery.staticTexts["Open Pull Request"].tap()
+        
+        
+    }
+    
+    func testSeeBranches() {
+        
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery.cells.containing(.staticText, identifier:"grit").staticTexts["mojombo"].tap()
+        tablesQuery.staticTexts["Branches"].tap()
+
+        
+    }
+    
     
 }
